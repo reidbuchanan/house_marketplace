0e31c69640ec3e16591ab0ac1a8293ba23e6e6d7
Reid Buchanan <reidbuchanan@gmail.com>
2022-11-26 15:59:16 -0800

    finally figured out what was going on with not being able to connect to firebase ended up creating whole new project and now am able to see listings and add listings

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,23 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhPZKUvqp9jKrN-Gbo3dJKc8__A28QjjA",
-  authDomain: "house-marketplace-app-1206b.firebaseapp.com",
-  projectId: "house-marketplace-app-1206b",
-  storageBucket: "house-marketplace-app-1206b.appspot.com",
-  messagingSenderId: "987120952771",
-  appId: "1:987120952771:web:30318b3546a278aa6e4976",
-  measurementId: "G-LFHJH07679",
+  apiKey: "AIzaSyCJzEdXika3nnlpQ8q6qiyiG5DJTZ0Q2mo",
+  authDomain: "housingmarketplace2.firebaseapp.com",
+  projectId: "housingmarketplace2",
+  storageBucket: "housingmarketplace2.appspot.com",
+  messagingSenderId: "125287716739",
+  appId: "1:125287716739:web:cd9345856ae96c7a8288b6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore();
+export const db = getFirestore()
